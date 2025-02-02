a813cd20ce8d6e0bd5e558974abfc03494e9eee8
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2025-02-02 13:53:01 +0200

    impeleminting models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id', 'total_amount', 'status', 'shipping_address',
+    ];
+
+    public function user() // each order belong to a user
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems() // order have emany items 
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name', 'description', 'price', 'category_id', 'image_url', 'stock_quantity',
+    ];
+
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+
 }
