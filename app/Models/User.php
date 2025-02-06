23c00c256e5040597373169c083f29a96385e721
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2025-02-06 17:03:00 +0200

    instll brezzz

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
         return $this->hasOne(Cart::class, 'user_id', 'id');
     }
 
+    public function isAdmin() {
+        return $this->role === 'admin';
+    }
+
 }
