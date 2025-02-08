4c499f6c2970ebcc1869ccdf46329c6f7664cf61
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2025-02-08 16:25:50 +0200

    ADD METT EXCEL

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -30,6 +30,8 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+
+        dd($request);
         // $request->validate([$request->all()]);
         $request->validate([
             'name' => 'required|string|max:255',
